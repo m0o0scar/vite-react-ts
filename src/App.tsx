fa4ecef9481eb872d13ec9406bba2559343255b6
Oscar <moscartong@gmail.com>
2022-01-26 15:25:06 +0800

    feat: add styled-component & recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { RecoilRoot } from 'recoil';
+import { GlobalStyle } from './components/GlobalStyle';
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <RecoilRoot>
+        <GlobalStyle />
+        <h1>Hello World</h1>
+      </RecoilRoot>
+    </>
+  )
 }
