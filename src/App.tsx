6e0d8916b0f9e7218092f252c1a4b9ead910026a
Oscar <moscartong@gmail.com>
2022-01-26 17:50:50 +0800

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { RecoilRoot } from 'recoil';
+import { Link, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from './components/GlobalStyle';
+
+const Home = () => <p>this is <i>home</i> page</p>;
+const About = () => <p>this is <b>about</b> page</p>;
 
 export function App() {
   return (
     <>
-      <RecoilRoot>
-        <GlobalStyle />
-        <h1>Hello World</h1>
-      </RecoilRoot>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+      <p>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </p>
     </>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from 'recoil';
 import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
